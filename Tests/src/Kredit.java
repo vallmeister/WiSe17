a610c72200208d682e09ff2276e4ac1e1798cf05
Christian Valenti <christian.valenti@uni-ulm.de>
2017-11-19 14:41:18 +0100

    Suche task from SPOX

---
+++ Tests/src/Kredit.java
@@ -0,0 +1,19 @@
+public class Kredit {
+
+  /**
+   * Johannes task
+   * @param args no needed.
+   */
+  public static void main(String[] args) {
+    int loan = 10000;
+    double interests = 10;
+    interests /= 1200;
+    interests += 1;
+    System.out.println(interests);
+    while (loan > 1000) {
+      loan *= interests;
+      loan -= 1000;
+      System.out.println(loan);
+    }
+  }
+}
