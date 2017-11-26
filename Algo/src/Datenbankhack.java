05bc6aa0587d207a6c9a5436f275b51b47bb68b4
Christian Valenti <christian.valenti@uni-ulm.de>
2017-11-26 22:41:56 +0100

    Sheet 4 spox

--- Algo/src/Datenbankhack.java
+++ Algo/src/Datenbankhack.java
@@ -20,7 +20,7 @@ class Datenbankhack {
     try {
       buffrd = new BufferedReader(new InputStreamReader(System.in));
       enterprises = Integer.parseInt(buffrd.readLine());
-      int hashtable;
+      int hashtableSize;
       int givenKeysAmount;
       int searchedKey;
       String[] deezInts;
@@ -30,18 +30,32 @@ class Datenbankhack {
 
         // reading those ints
         deezInts = buffrd.readLine().split(" ");
-        hashtable = Integer.parseInt(deezInts[0]);
+        hashtableSize = Integer.parseInt(deezInts[0]);
         givenKeysAmount = Integer.parseInt(deezInts[1]);
         searchedKey = Integer.parseInt(deezInts[2]);
+        int[] givenKeys = new int[givenKeysAmount];
 
         // hashing
         for (int j = 0; j < givenKeysAmount; ++j) {
-
+          givenKeys[j] = buffrd.read();
         }
-
+        linearHashing(givenKeys, hashtableSize);
       }
     } catch (IOException exception) {
       exception.printStackTrace();
     }
+  }
+
+  public static void linearHashing(int[] keys, int hashtableSize) {
+    int[] hashtable = new int[hashtableSize];
+
+    // hashing the keys
+    for (int i = 0; i < keys.length; ++i) {
+      if (hashtable[(keys[i] % hashtableSize)] != 0);
+    }
+  }
+
+  public static void doubleHashing() {
+
   }
 }
---
+++ Tests/src/arrayTest.java
@@ -0,0 +1,8 @@
+public class arrayTest {
+  public static void main(String[] args) {
+    int[] test = new int[4];
+    for (int i = 0; i < 4; ++i) {
+      System.out.println(test[i]);
+    }
+  }
+}
