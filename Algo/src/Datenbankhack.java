72e9a430a93121fd3a52d6a50232fb14685226d0
Christian Valenti <christian.valenti@uni-ulm.de>
2017-11-29 22:43:34 +0100

    checkstyle fixed

--- Algo/src/Datenbankhack.java
+++ Algo/src/Datenbankhack.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
  * Sheet 4 task 1. Christian Valenti.
  */
 class Datenbankhack {
+  private static String output = "";
 
   /**
    * main method to read input from SPOX.
@@ -13,7 +14,6 @@ class Datenbankhack {
    */
   public static void main(String[] args) {
     BufferedReader buffrd;
-    String input;
     int enterprises;
 
     // try to read input
@@ -37,25 +37,60 @@ class Datenbankhack {
 
         // hashing
         for (int j = 0; j < givenKeysAmount; ++j) {
-          givenKeys[j] = buffrd.read();
+          givenKeys[j] = Integer.parseInt(buffrd.readLine());
         }
-        linearHashing(givenKeys, hashtableSize);
+        linearHashing(givenKeys, hashtableSize, searchedKey);
+        doubleHashing(givenKeys, hashtableSize, searchedKey);
+        output += "\n";
+        System.out.println(output);
       }
     } catch (IOException exception) {
       exception.printStackTrace();
     }
   }
 
-  public static void linearHashing(int[] keys, int hashtableSize) {
+  private static void linearHashing(int[] keys, int hashtableSize, int searchedKey) {
     int[] hashtable = new int[hashtableSize];
 
-    // hashing the keys
+    // initialise the hashtable
+    for (int i = 0; i < hashtableSize; ++i) {
+      hashtable[i] = - 1;
+    }
+
+    // hashing
     for (int i = 0; i < keys.length; ++i) {
-      if (hashtable[(keys[i] % hashtableSize)] != 0);
+      int count = 0;
+      while (hashtable[(keys[i] + count) % hashtableSize] != - 1) {
+        count++;
+      }
+      hashtable[(keys[i] + count) % hashtableSize] = keys[i];
+      if (searchedKey == keys[i]) {
+        output += (keys[i] % hashtableSize) + "\n";
+      }
     }
   }
 
-  public static void doubleHashing() {
+  private static void doubleHashing(int[] keys, int hashtableSize, int searchedKey) {
+    int[] hashtable = new int[hashtableSize];
 
+    // initialise the hashtable
+    for (int i = 0; i < hashtableSize; ++i) {
+      hashtable[i] = - 1;
+    }
+
+    // hashing
+    for (int i = 0; i < keys.length; ++i) {
+      int count = 0;
+      while (hashtable[(keys[i] + count * (1 + (keys[i] % (hashtableSize - 1)))) % hashtableSize]
+          != -1) {
+        count++;
+      }
+      hashtable[(keys[i] + count * (1 + (keys[i] % (hashtableSize - 1))))  % hashtableSize]
+          = keys[i];
+      if (searchedKey == keys[i]) {
+        output += ((keys[i] + count * (1 + (keys[i] % (hashtableSize - 1)))) % hashtableSize)
+            + "\n";
+      }
+    }
   }
 }
