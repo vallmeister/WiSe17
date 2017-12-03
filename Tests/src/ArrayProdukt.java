208d54d23f7e26be1bdbf384fe4c4910c196115b
Christian Valenti <christian.valenti@uni-ulm.de>
2017-12-03 13:50:43 +0100

    SPOX Sheet 4

---
+++ Tests/src/ArrayProdukt.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class ArrayProdukt {
+
+  private static int[] array1 = {1, 2, 3, 4, 5, 6, 7};
+  /**
+   * task for Hummler.
+   */
+  public static void main(String[] args) {
+    int[] result = produkte();
+    System.out.println(Arrays.toString(result));
+  }
+
+  /**
+   * getting the products
+   */
+  private static int[] produkte() {
+    int[] result = new int[array1.length];
+    for (int i = 0; i < array1.length; ++i) {
+      int product = 1;
+      for (int j = 0; j < array1.length; ++j) {
+        if (j == i) {
+          continue;
+        }
+        product *= array1[j];
+      }
+      result[i] = product;
+    }
+    return result;
+  }
+}
