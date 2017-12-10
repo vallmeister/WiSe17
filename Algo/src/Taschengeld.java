43e401a2dea707a59a72ca965fc85cc70e79e400
Christian Valenti <christian.valenti@uni-ulm.de>
2017-12-10 22:44:32 +0100

    Spox not solved. Added txt for Grundkurs

---
+++ Algo/src/Taschengeld.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Christian Valenti 959042.
+ * Algorithmen sheet 6 SPOX task.
+ */
+class Taschengeld {
+
+  /**
+   * main method of application.
+   * @param args no arguments needed.
+   */
+  public static void main(String[] args) {
+    String input; // read lines
+    String[] toMaximum; // splitting input and calculate the maximum
+    String[] toMinimum; // " " minimum
+    BufferedReader buffrd;  // used to read SPOX-input
+    int tests;  // amount of cases to be calculated
+
+    // read input
+    try {
+      buffrd = new BufferedReader(new InputStreamReader(System.in));
+      input = buffrd.readLine();
+      tests = Integer.parseInt(input);
+
+      // iterating the procedure for each test case
+      for (int i = 0; i < tests; ++i) {
+        input = buffrd.readLine();
+
+      }
+    } catch (IOException noInput) {
+      noInput.printStackTrace();
+    }
+  }
+}
