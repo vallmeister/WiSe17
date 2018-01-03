4b981d62bf8cadf5999787dc8ae7273ff3401596
Christian Valenti <christian.valenti@uni-ulm.de>
2018-01-03 19:50:05 +0100

    removed PvS

--- PvS/src/Sheet01/api/BirdOfPrey.java
+++
@@ -1,6 +0,0 @@
-package Sheet01.api;
-
-/** Interface for birds of prey */
-public interface BirdOfPrey {
-  public void hunt();
-}
--- PvS/src/Sheet01/api/Moveable.java
+++
@@ -1,7 +0,0 @@
-package Sheet01.api;
-
-/** Things that can move */
-public interface Moveable {
-
-  public void move();
-}
